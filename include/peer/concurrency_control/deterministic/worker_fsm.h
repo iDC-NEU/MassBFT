a6a3b4f61da8eb8b818f10e8dd187fe2851e2710
peng <1015192446qq.com>
2023-02-20 05:12:17 -0800

    add worker state machine with tests

---
+++ include/peer/concurrency_control/deterministic/worker_fsm.h
@@ -0,0 +1,138 @@
+//
+// Created by peng on 2/20/23.
+//
+
+#pragma once
+
+#include "bthread/butex.h"
+
+#include "glog/logging.h"
+
+#include <memory>
+#include <functional>
+#include <thread>
+
+namespace peer::cc {
+
+    // for invoker
+    enum class InvokerCommand {
+        IDLE = 0,       // default state
+        START = 1,      // initialization
+        EXEC = 2,       // start the execution stage of the protocol
+        COMMIT = 3,     // start the validation stage of the protocol
+        EXIT = 4,       // exit the worker
+    };
+
+    // for worker
+    enum class ReceiverState {
+        READY = 0,          // worker is initializing or is free
+        FINISH_EXEC = 1,    // worker finish execution phase
+        FINISH_COMMIT = 2,  // worker finish validate phase
+        EXITED = 3,         // worker is about to exit, use join func to safe delete it.
+    };
+
+    class WorkerFSM {
+    public:
+        virtual ~WorkerFSM() = default;
+
+        WorkerFSM() = default;
+
+        WorkerFSM(const WorkerFSM&) = delete;
+
+        WorkerFSM(WorkerFSM&&) = delete;
+
+        virtual ReceiverState OnCreate() = 0;
+
+        virtual ReceiverState OnDestroy() = 0;
+
+        virtual ReceiverState OnExecuteTransaction() = 0;
+
+        virtual ReceiverState OnCommitTransaction() = 0;
+
+    };
+
+    class Worker {
+    public:
+        static std::unique_ptr<Worker> NewWorker(std::shared_ptr<WorkerFSM> fsm) {
+            if (!fsm) {
+                LOG(ERROR) << "WorkerFSM pointer is nil!";
+                return nullptr;
+            }
+            std::unique_ptr<Worker> worker(new Worker());
+            worker->_fsm = std::move(fsm);
+            return worker;
+        }
+
+        virtual ~Worker() {
+            _commandCallback = nullptr;
+            execute(InvokerCommand::EXIT);
+            if (_tid) { _tid->join(); }
+            bthread::butex_destroy(_command);
+        }
+
+        Worker(const Worker &) = delete;
+
+        Worker(Worker &&) = delete;
+
+        // set a command to worker, response is in callback
+        int execute(InvokerCommand command) {
+            _command->store(static_cast<int>(command), std::memory_order_relaxed);
+            return bthread::butex_wake_all(_command);
+        }
+
+        void setCommandCallback(std::function<void(ReceiverState)> commandCallback) {
+            _commandCallback = std::move(commandCallback);
+        }
+
+        bool checkAndStartService() {
+            if (!_fsm) {
+                LOG(ERROR) << "WorkerFSM pointer is nil!";
+                return false;
+            }
+            execute(InvokerCommand::START);
+            _tid = std::make_unique<std::thread>(&Worker::run, this);
+            return true;
+        }
+
+    protected:
+        void run() {
+            int cEnum = static_cast<int>(InvokerCommand::IDLE);
+            do {
+                while(_command->compare_exchange_strong(cEnum, cEnum, std::memory_order_acquire, std::memory_order_relaxed)) {
+                    bthread::butex_wait(_command, cEnum, nullptr);
+                }
+                ReceiverState ret = ReceiverState::READY;  // return value of command
+                switch (static_cast<InvokerCommand>(cEnum)) {
+                    case InvokerCommand::IDLE:
+                        continue;   // goto the front, retry;
+                    case InvokerCommand::START:
+                        ret = _fsm->OnCreate();
+                        break;
+                    case InvokerCommand::EXEC:
+                        ret = _fsm->OnExecuteTransaction();
+                        break;
+                    case InvokerCommand::COMMIT:
+                        ret = _fsm->OnCommitTransaction();
+                        break;
+                    case InvokerCommand::EXIT:
+                        ret = _fsm->OnDestroy();
+                        break;
+                }
+                if (_commandCallback) {
+                    _commandCallback(ret);
+                }
+            } while (static_cast<InvokerCommand>(cEnum) != InvokerCommand::EXIT);
+        }
+
+    protected:
+        Worker() { _command = bthread::butex_create_checked<butil::atomic<int>>(); }
+
+    private:
+        std::unique_ptr<std::thread> _tid;
+        std::function<void(ReceiverState)> _commandCallback;
+        // the fsm may be shared among workers
+        std::shared_ptr<WorkerFSM> _fsm;
+        butil::atomic<int> *_command;
+    };
+
+}
---
+++ tests/peer/concurrency_control/deterministic/worker_fsm.cpp
@@ -0,0 +1,76 @@
+//
+// Created by peng on 2/20/23.
+//
+
+#include "gtest/gtest.h"
+#include "glog/logging.h"
+
+#include "bthread/countdown_event.h"
+#include "peer/concurrency_control/deterministic/worker_fsm.h"
+
+class WorkerTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    };
+
+    void TearDown() override {
+    };
+
+};
+
+class MockFSM: public peer::cc::WorkerFSM {
+public:
+    peer::cc::ReceiverState OnCreate() override {
+        state = peer::cc::ReceiverState::READY;
+        return state;
+    }
+
+    peer::cc::ReceiverState OnDestroy() override {
+        state = peer::cc::ReceiverState::EXITED;
+        return state;
+
+    }
+    peer::cc::ReceiverState OnExecuteTransaction() override {
+        state = peer::cc::ReceiverState::FINISH_EXEC;
+        return state;
+
+    }
+    peer::cc::ReceiverState OnCommitTransaction() override {
+        state = peer::cc::ReceiverState::FINISH_COMMIT;
+        return state;
+
+    }
+
+    peer::cc::ReceiverState state{};
+};
+
+
+TEST_F(WorkerTest, TestSignalSendReceive) {
+    auto fsm = std::make_shared<MockFSM>();
+    auto worker = peer::cc::Worker::NewWorker(fsm);
+    ASSERT_TRUE(fsm != nullptr && worker != nullptr);
+    // setup worker
+    bthread::CountdownEvent cd(1);
+    std::atomic<peer::cc::ReceiverState> state{};
+    worker->setCommandCallback([&](peer::cc::ReceiverState ret) {
+        state.store(ret, std::memory_order_acq_rel);
+        cd.signal(1);
+    });
+    ASSERT_TRUE(worker->checkAndStartService());
+    // emit signal
+    worker->execute(peer::cc::InvokerCommand::START);
+    cd.wait(); cd.reset(1);
+    ASSERT_TRUE(state == peer::cc::ReceiverState::READY);
+    for (int i=0; i< 100; i++) {
+        worker->execute(peer::cc::InvokerCommand::EXEC);
+        cd.wait(); cd.reset(1);
+        ASSERT_TRUE(state == peer::cc::ReceiverState::FINISH_EXEC);
+
+        worker->execute(peer::cc::InvokerCommand::COMMIT);
+        cd.wait(); cd.reset(1);
+        ASSERT_TRUE(state == peer::cc::ReceiverState::FINISH_COMMIT);
+    }
+    worker->execute(peer::cc::InvokerCommand::EXIT);
+    cd.wait(); cd.reset(1);
+    ASSERT_TRUE(state == peer::cc::ReceiverState::EXITED);
+}

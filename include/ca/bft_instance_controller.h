0d6d4a0e1ee36d5ba499b8f5ada80eac731b766e
sydxsty <1015192446@qq.com>
2023-04-24 17:03:44 +0800

    code refactor

--- include/ca/bft_instance_controller.h
+++ include/ca/bft_instance_controller.h
@@ -4,46 +4,103 @@
 
 #pragma once
 
-#include <utility>
-#include <memory>
-#include <functional>
-
+#include "common/property.h"
 #include "common/ssh.h"
+#include "common/timer.h"
+#include <filesystem>
 
+namespace ca {
+    struct SSHConfig {
+        std::string ip;
+        int port=-1;
+        std::string userName;
+        std::string password;
+    };
 
-namespace util{
-    class BFTInstanceController{
+    class BFTInstanceController {
     public:
-        // prepare necessary message for connection to host
-        static std::unique_ptr<BFTInstanceController> NewBFTInstanceController(const std::string& ip, const std::string& user, const std::string &password,  int port=-1);
+        // Try to establish an ssh session with remote host
+        static std::unique_ptr<BFTInstanceController> NewBFTInstanceController(SSHConfig config,
+                                                                               int processId,
+                                                                               std::filesystem::path runningPath,
+                                                                               std::filesystem::path jvmExec) {
+            std::unique_ptr<BFTInstanceController> controller(new BFTInstanceController(std::move(config), processId));
+            auto session = util::SSHSession::NewSSHSession(controller->_config.ip, controller->_config.port);
+            if(session == nullptr) {
+                return nullptr;
+            }
+            if(!session->connect(controller->_config.userName, controller->_config.password)) {
+                return nullptr;
+            }
+            controller->_session = std::move(session);
+            controller->_runningPath = std::move(runningPath);
+            controller->_jvmExec = std::move(jvmExec);
+            return controller;
+        }
 
-        ~BFTInstanceController() = default;
+        BFTInstanceController(const BFTInstanceController &) = delete;
 
-        BFTInstanceController(const BFTInstanceController&) = delete;
+        BFTInstanceController(BFTInstanceController &&) = delete;
 
-        BFTInstanceController(BFTInstanceController&&) = delete;
-
-        // start specific host by processId
-        bool StartInstance(const std::string &command, int processId);
+        bool startInstance() {
+            std::vector<std::string> builder {
+                    "cd",
+                    _runningPath.string(),
+                    "&&",
+                    _jvmExec.string(),
+                    "-Dlogback.configurationFile=./config/logback.xml",
+                    "-classpath",
+                    "./nc_bft.jar bftsmart.demo.neuchainplus.NeuChainServer",
+                    std::to_string(_processId),
+            };
+            std::string command;
+            for (const auto& it: builder) {
+                command.append(it).append(" ");
+            }
+            DLOG(INFO) << "The execute command: " << command;
+            auto channel = this->_session->createChannel();
+            if(channel == nullptr) {
+                return false;
+            }
+            _bftInstanceChannel = std::move(channel);
+            waitUntil = util::Timer::time_now_ms() + 10 * 1000;
+            return _bftInstanceChannel->execute(command);
+        }
 
         // read feedback from specific instance by processId
-        bool readFeedback(std::string& buf, int errFlag, const std::function<bool(std::string_view append)>& callback, int processId);
+        std::tuple<bool, std::string, std::string> getChannelResponse() {
+            auto callback = [this](std::string_view sv) {
+                auto spanMs = waitUntil - util::Timer::time_now_ms();
+                util::Timer::sleep_ns(spanMs * 1000 * 1000);
+                if (sv.empty()) {
+                    return false;
+                }
+                LOG(INFO) << sv;
+                return true;
+            };
+            std::string out, error;
+            if (_bftInstanceChannel == nullptr) {
+                return {false, {}, {}};
+            }
+            // do not catch the return value!
+            _bftInstanceChannel->read(out, false, callback);
+            _bftInstanceChannel->read(error, true, callback);
+            return {true, out, error};
+        }
 
-        [[nodiscard]] std::string getIP() const { return this->_ip; };
-
-        [[nodiscard]] int getPort() const { return this->_port; };
-
-        [[nodiscard]] std::string getUser() const { return this->_user; };
+        [[nodiscard]] const auto& getConfig() const { return _config; };
 
     protected:
-        BFTInstanceController() = default;
+        explicit BFTInstanceController(auto&& config, int processId)
+                : _config(std::forward<decltype(config)>(config)), _processId(processId) { }
 
     private:
-        std::unique_ptr<SSHSession> _session;
-        // _channel store the channel at the processId(value) position
-        std::vector<std::unique_ptr<SSHChannel>> _channels;
-        std::string _ip;
-        int _port = -1;
-        std::string _user;
+        const SSHConfig _config;
+        const int _processId;
+        int64_t waitUntil = -1;
+        std::unique_ptr<util::SSHSession> _session;
+        std::unique_ptr<util::SSHChannel> _bftInstanceChannel;
+        std::filesystem::path _runningPath;
+        std::filesystem::path _jvmExec;
     };
 }
--- tests/ca/bft_instance_controller_tests.cpp
+++ tests/ca/bft_instance_controller_tests.cpp
@@ -1,56 +1,43 @@
 //
 // Created by user on 23-4-16.
 //
+
+#include "ca/bft_instance_controller.h"
+
 #include "gtest/gtest.h"
 #include "glog/logging.h"
-#include "ca/bft_instance_controller.h"
-#include "common/timer.h"
 
 class ControllerTest : public ::testing::Test {
 protected:
+    ControllerTest() {
+        sshConfig.userName = "user";
+        sshConfig.password = "123456";
+        sshConfig.ip = "127.0.0.1";
+        sshConfig.port = -1;
+    }
+
     void SetUp() override {
     };
 
     void TearDown() override {
     };
 
-
 protected:
-    constexpr static auto userName = "user";
-    constexpr static auto passWord = "123456";
-    constexpr static auto remoteIp = "127.0.0.1";
-    inline static std::string runningPath = "/home/user/nc_bft/";
-    inline static std::string jvmPath = "/home/user/.jdks/openjdk-20/bin/java ";
-    inline static std::string jvmOption = "-Dlogback.configurationFile=./config/logback.xml ";
-    inline static std::string classPath = "-classpath ./nc_bft.jar ";
-    inline static std::string runCommand = "cd " + runningPath + "&&" + jvmPath.append(jvmOption).append(classPath) + "bftsmart.demo.neuchainplus.NeuChainServer " ;
-
-    static bool readChannelCallback(std::string_view sv) {
-        util::Timer::sleep_sec(1);
-        if (sv.empty()) {
-            return false;
-        }
-        LOG(INFO) << sv;
-        return true;
-    };
-
+    ca::SSHConfig sshConfig;
+    inline static std::string runningPath = "/home/user/nc_bft";
+    inline static std::string jvmPath = "/home/user/.jdks/openjdk-20/bin/java";
 };
 
-TEST_F(ControllerTest, StartTest){
-    auto controller = util::BFTInstanceController::NewBFTInstanceController(remoteIp, userName, passWord);
-    auto ret = controller->StartInstance(runCommand, 0);
-    ASSERT_TRUE(ret);
-    ret = controller->StartInstance(runCommand, 1);
-    ASSERT_TRUE(ret);
-    ret = controller->StartInstance(runCommand, 2);
-    ASSERT_TRUE(ret);
-    ret = controller->StartInstance(runCommand, 3);
-    ASSERT_TRUE(ret);
-
-    std::string out, error;
-    controller->readFeedback(out, false, readChannelCallback, 0);
-    controller->readFeedback(error, true, readChannelCallback, 0);
-    controller->readFeedback(out, false, readChannelCallback, 1);
-    controller->readFeedback(error, true, readChannelCallback, 1);
-    sleep(10);
+TEST_F(ControllerTest, StartTest) {
+    std::vector<std::unique_ptr<ca::BFTInstanceController>> ctlList(4);
+    for (int i=0; i<4; i++) {
+        ctlList[i] = ca::BFTInstanceController::NewBFTInstanceController(sshConfig, i, runningPath, jvmPath);
+        ASSERT_TRUE(ctlList[i]->startInstance());
+    }
+    for (int i=0; i<4; i++) {
+        auto[success, out, err] = ctlList[i]->getChannelResponse();
+        ASSERT_TRUE(success);
+        LOG(INFO) << out << err;
+    }
+    util::Timer::sleep_sec(5);
 }

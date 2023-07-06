8229e93aca5b22067c62beaa72d84aedffea1792
sydxsty <1015192446@qq.com>
2023-07-06 13:04:43 +0800

    Add comment

--- src/peer/core/module_coordinator.cpp
+++ src/peer/core/module_coordinator.cpp
@@ -97,8 +97,10 @@ namespace peer::core {
             return false;
         }
         realBlock->getSerializedMessage()->clear(); // out of date (contains no exec result)
+        if (_localNode->nodeId == 0) {
+            LOG(INFO) << "Leader of local group " << _localNode->groupId << " commit a block, chainId: " << regionId  << ", blockId: " << blockId;
+        }
         // notify user by rpc
-        DLOG(INFO) << _localNode->groupId << ", " << _localNode->nodeId << " pushBlock, chainId: " << regionId  << ", blockId: " << blockId;
         _userRPCNotifier->insertBlockAndNotify(regionId, std::move(realBlock));
         return true;
     }

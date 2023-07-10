97a95ee1e77186f5e82e78838d0f62d08323505d
sydxsty <1015192446@qq.com>
2023-07-10 21:14:12 +0800

    Bug fix

--- standalone/ycsb/main.cpp
+++ standalone/ycsb/main.cpp
@@ -5,6 +5,9 @@
 #include "ycsb/engine.h"
 
 int main(int, char *[]) {
+    util::OpenSSLSHA256::initCrypto();
+    util::OpenSSLED25519::initCrypto();
+    util::Properties::LoadProperties("peer.yaml");
     auto* p = util::Properties::GetProperties();
     ycsb::YCSBEngine engine(*p);
     engine.startTest();

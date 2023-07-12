903e8108cd02dd8e3d5f9afe82670d82ef47fbf2
sydxsty <1015192446@qq.com>
2023-07-12 13:57:31 +0800

    Refactor compile command

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
-# make sure the nc-bft package is under ../
 
 export DEBIAN_FRONTEND=noninteractive
-
 apt-get update
 apt-get install -y zip unzip git cmake libtool make autoconf g++-11 zlib1g-dev libgoogle-perftools-dev g++ openssh-server
-
-# the zip is already decompressed by ca
-# unzip -q ../nc_bft.zip ../
-
-chmod +x ../nc_bft/clash-linux-amd64-v3
-nohup ../nc_bft/clash-linux-amd64-v3 -f ../nc_bft/proxy.yaml &
-
-export https_proxy=http://127.0.0.1:7890;
-export http_proxy=http://127.0.0.1:7890;
-export all_proxy=socks5://127.0.0.1:7890;
-
-# wait for clash ready
-sleep 5
 
 # build the binary
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc-11 -DCMAKE_CXX_COMPILER=/usr/bin/g++-11 -B build

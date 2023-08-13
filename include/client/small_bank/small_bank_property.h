52e20f9eec384c73938ae4ce410bfce5e0329446
sydxsty <1015192446@qq.com>
2023-08-13 20:02:52 +0800

    bug fix

--- include/client/small_bank/small_bank_property.h
+++ include/client/small_bank/small_bank_property.h
@@ -34,7 +34,7 @@ namespace client::small_bank {
 
         constexpr static const auto BALANCE_PROPORTION = "bal_proportion";
         constexpr static const auto DEPOSIT_CHECKING_PROPORTION = "dc_proportion";
-        constexpr static const auto TRANSACT_SAVING_PROPORTION = "rs_proportion";
+        constexpr static const auto TRANSACT_SAVING_PROPORTION = "ts_proportion";
         constexpr static const auto AMALGAMATE_PROPORTION = "ang_proportion";
         constexpr static const auto WRITE_CHECK_PROPORTION = "wc_proportion";
 

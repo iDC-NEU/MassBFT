b1ab142add0b61deed9dba95aeb6d23e1f52324f
sydxsty <1015192446@qq.com>
2023-09-15 16:24:59 +0800

    switch to new raft api

--- include/common/raft/raft_fsm.h
+++ include/common/raft/raft_fsm.h
@@ -138,6 +138,11 @@ namespace util::raft {
             DLOG(INFO) << "address " << address << " commit conf: " << conf << " at index " << index;
         }
 
+        bool on_follower_receive(int term, int index, const ::butil::IOBuf& data) override {
+            DLOG(INFO) << "follower " << address << " receive index: " << index << " at term: " << term << ", data size: " << data.size();
+            return true;    // accept it
+        }
+
     private:
         butil::EndPoint address;
         RaftLog<butil::IOBuf> logs;

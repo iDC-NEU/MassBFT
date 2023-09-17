e6db3a09a3a0ab930645deb364e711f66dfd68e5
sydxsty <1015192446@qq.com>
2023-09-17 17:12:52 +0800

    Add aliyun vm creator

--- include/common/bccsp.h
+++ include/common/bccsp.h
@@ -237,12 +237,12 @@ namespace util {
             if (cache.if_contains(ski, [&](CacheType::value_type& value) { key = value.second; })) {
                 return key;
             }
+            auto ret = storage->loadKey(ski);
+            if (!ret) { // we cant find it in cache or storage
+                return nullptr;
+            }
             auto notExist = [&](const CacheType::constructor& ctor) {
                 // load from disk
-                auto ret = storage->loadKey(ski);
-                if (!ret) { // we cant find it in cache or storage
-                    return false;
-                }
                 auto [raw, isPrivate] = std::move(*ret);
                 key = GetKeyFromRaw(ski, raw, isPrivate, false);
                 ctor(ski, key);

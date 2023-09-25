4e7eb4783bcfbdc1b1b5b2af7e343b87c9cb402f
sydxsty <1015192446@qq.com>
2023-09-25 14:54:23 +0800

    Separate status and invoke server

--- src/client/core/db.cpp
+++ src/client/core/db.cpp
@@ -14,32 +14,33 @@ namespace client::core {
     DBStatus::~DBStatus() = default;
 
     DBFactory::DBFactory(const util::Properties &p) {
-        server = p.getNodeProperties().getLocalNodeInfo();
+        invokeServer = p.getNodeProperties().getLocalNodeInfo();
+        queryServer = p.getNodeProperties().getLocalNodeInfo();
         // calculate port
         portConfig = util::ZMQPortUtil::InitLocalPortsConfig(p);
         // init bccsp
         auto node = p.getCustomPropertiesOrPanic("bccsp");
         bccsp = std::make_unique<util::BCCSP>(std::make_unique<util::YAMLKeyStorage>(node));
         CHECK(bccsp) << "Can not init bccsp";
-        auto port = portConfig->getLocalServicePorts(util::PortType::USER_REQ_COLLECTOR)[server->nodeId];
-        dbc = ::client::NeuChainDBConnection::NewNeuChainDBConnection(server->priIp, port);
+        auto port = portConfig->getLocalServicePorts(util::PortType::USER_REQ_COLLECTOR)[invokeServer->nodeId];
+        dbc = ::client::NeuChainDBConnection::NewNeuChainDBConnection(invokeServer->priIp, port);
     }
 
     std::unique_ptr<DB> DBFactory::newDB() const {
-        auto priKey = bccsp->GetKey(server->ski);
+        auto priKey = bccsp->GetKey(invokeServer->ski);
         if (!priKey || !priKey->Private()) {
             return nullptr;
         }
-        return std::make_unique<client::NeuChainDB>(server, dbc, std::move(priKey));
+        return std::make_unique<client::NeuChainDB>(invokeServer, dbc, std::move(priKey));
     }
 
     std::unique_ptr<DBStatus> DBFactory::newDBStatus() const {
-        auto priKey = bccsp->GetKey(server->ski);
+        auto priKey = bccsp->GetKey(queryServer->ski);
         if (!priKey || !priKey->Private()) {
             return nullptr;
         }
-        auto port = portConfig->getLocalServicePorts(util::PortType::BFT_RPC)[server->nodeId];
-        return std::make_unique<client::NeuChainStatus>(server, port, std::move(priKey));
+        auto port = portConfig->getLocalServicePorts(util::PortType::BFT_RPC)[queryServer->nodeId];
+        return std::make_unique<client::NeuChainStatus>(queryServer, port, std::move(priKey));
     }
 
     DBFactory::~DBFactory() = default;
